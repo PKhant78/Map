2af715a9a724c3c20286e9db6a96e93eb5aeb733
Bryanm29 <bmilien523@gmail.com>
2024-11-21 15:18:43 -0500

    update-6
    Check out ScaleInterfaceFix. Deleted any duplicate files that appeared in different directories (i.e. Icons in the same path as Assets and ALSO INSIDE Assets, so there were two folders of the same name containing sometimes  different, sometimes identical content)

--- Assets/HighlightShowHide.cs
+++ Assets/HighlightShowHide.cs
@@ -9,7 +9,11 @@ public class HighlightShowHide : MonoBehaviour
     {
         button.GetComponent<Image>().color = new Color(1, 32f / 255f, 152f / 255f);
         objectPlacement.transform.localPosition = new Vector3(960, 0, 0);
-        objectScale.transform.localPosition = new Vector3(-900, -350, 0);
+        objectScale.transform.position = new Vector3(200, 62.5f, 0);
+        CanvasScaler canvasScale = objectScale.GetComponentInParent<CanvasScaler>();
+        float multiplier = canvasScale.referenceResolution.x / canvasScale.referenceResolution.y;
+        Vector2 divisor = new Vector2(20f, 20f);
+        objectScale.transform.position += new Vector3((canvasScale.referenceResolution.x / divisor.x), (canvasScale.referenceResolution.y / divisor.y * multiplier), 0);
         saveLoad.transform.localPosition = new Vector3(960, -730, 0);
     }
     [SerializeField] private GameObject saveLoad;
